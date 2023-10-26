037a77194e2d03455c5207909a4acc8d9108dd06
Oliver V. Vammen <47665@student.eadania.dk>
2023-10-26 09:49:46 +0200

    Fjernet kode i js

--- js_files/file.js
+++ js_files/file.js
@@ -1,5 +1,3 @@
-document.getElementById("demo").innerHTML = "It's a beautiful day :D";
-
 alert("Click here if you wanna continue :D");
 
-document.getElementById("demo").innerHTML.replace = "You're gay!!!";
+document.getElementById("demo").innerHTML = "You're gay!!!";
