396d6f860fa7bb1d6eaf9a912f75a3d90e34c73a
Oliver V. Vammen <47665@student.eadania.dk>
2023-10-26 09:44:50 +0200

    Opdatering til js

--- js_files/file.js
+++ js_files/file.js
@@ -1,3 +1,5 @@
+document.getElementById("demo").innerHTML = "It's a beautiful day :D";
+
 alert("Click here if you wanna continue :D");
 
 document.getElementById("demo").innerHTML = "You're gay!!!";
